ebddbf8c05f8516b2d0b0798b5fa8185990be492
Szymon Borucki <szymon.borucki@gmail.com>
2016-09-01 14:17:44 +0200

    enforce locale

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,8 @@ module RaceRegistration
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    I18n.default_locale = :pl
+    config.i18n.enforce_available_locales = false
+    config.i18n.available_locales = ["pl"]
+    config.i18n.default_locale = :'pl'
   end
 end
