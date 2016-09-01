e4a93859e6f468a5683be8999fd65cf78d8b5d78
Szymon Borucki <szymon.borucki@gmail.com>
2016-09-01 15:20:43 +0200

    css

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 
 module RaceRegistration
   class Application < Rails::Application
-    Rails.application.config.assets.precompile += %w(rails_admin/rails_admin.css rails_admin/rails_admin.js)
+    Rails.application.config.assets.precompile += %w(rails_admin.css rails_admin.js)
     config.middleware.use PDFKit::Middleware
     config.generators do |g|
       g.test_framework :rspec,
