a409d8158e6d18991587abb043f46d7a0776ff76
SimonBo <szymon.borucki@gmail.com>
2017-09-12 16:04:42 +0200

    update info

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -2,6 +2,10 @@ class Registration < ApplicationRecord
   validates :name, :age, :phone_number, :email, :address, :team_name, presence: true
   before_save :ensure_token
 
+  EVENT = "VI Rajdzie Rowerowym na Orientację"
+  NAME = 'Na jesiennym szlaku'
+  DATE = '8 października 2017'
+
   def ensure_token
     if token.blank?
     self.token = generate_token
