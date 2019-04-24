7c272d49e94b40877d641623ce0fff245b7006b2
szymon.borucki@gmail.com <szymon.borucki@gmail.com>
2019-04-24 16:17:24 +0200

    auth password only till 25.04

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  http_basic_authenticate_with name: 'schwarzenegger', password: 'arnold'
+  if Time.current < Time.new(2019, 04, 25)
+    http_basic_authenticate_with name: 'schwarzenegger', password: 'arnold'
+  end
 end
